88a693ba78bca54f690d4a19568074d4a8aaea8c
kateweb988 <katyavov2805@gmail.com>
2022-06-22 14:26:28 +0300

    pages2

--- gulpfile.js
+++ gulpfile.js
@@ -67,7 +67,7 @@ function styles() {
 }
 
 function sctipts() {
-	return gulp.src(['src/js/jquery-3.4.1.min.js', 'src/js/wow.min.js', 'src/js/main.js'])
+	return gulp.src(['src/js/jquery-3.4.1.min.js', 'src/js/wow.min.js', 'src/js/jquery.arcticmodal-0.3.min.js', 'src/js/main.js'])
 		.pipe(concat('all.js'))
 		.pipe(gulp.dest('dev/js/'))
 		.pipe(browserSync.stream());
--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,25 @@ document.addEventListener("DOMContentLoaded", () => {
 $(document).ready(function () {
    $('.js-example-basic-single').select2();
 });
+$('.articmodal-close').click(function (e) {
+   $.arcticmodal('close');
+
+});
+$('.calc').click(function (e) {
+   e.preventDefault();
+   $('#code').arcticmodal({
+   });
+});
+$('.p1').click(function (e) {
+   e.preventDefault();
+   $('#p1').arcticmodal({
+   });
+});
+$('.p5').click(function (e) {
+   e.preventDefault();
+   $('#success').arcticmodal({
+   });
+});
 $('body').on('click', '.password-control', function () {
    if ($('#password-input').attr('type') == 'password') {
       $(this).addClass('view');
@@ -150,23 +169,23 @@ $('.calc__btn_three').click(function (event) {
 // Accordeon
 $(document).ready(function () {
    $(".set > a").on("click", function () {
-     if ($(this).hasClass('active')) {
-       $(this).removeClass("active");
-       $(this).siblings('.content').slideUp(200);
-       $(".set > a i").removeClass("fa-minus").addClass("fa-plus");
-     }
-     else {
-       $(".set > a i").removeClass("fa-minus").addClass("fa-plus");
-       $(this).find("i").removeClass("fa-plus").addClass("fa-minus");
-       $(".set > a").removeClass("active");
-       $(this).addClass("active");
-       $('.content').slideUp(200);
-       $(this).siblings('.content').slideDown(200);
-     }
-     return false
+      if ($(this).hasClass('active')) {
+         $(this).removeClass("active");
+         $(this).siblings('.content').slideUp(200);
+         $(".set > a i").removeClass("fa-minus").addClass("fa-plus");
+      }
+      else {
+         $(".set > a i").removeClass("fa-minus").addClass("fa-plus");
+         $(this).find("i").removeClass("fa-plus").addClass("fa-minus");
+         $(".set > a").removeClass("active");
+         $(this).addClass("active");
+         $('.content').slideUp(200);
+         $(this).siblings('.content').slideDown(200);
+      }
+      return false
    });
- 
- });
+
+});
 jQuery('img.svg').each(function () {
    var $img = jQuery(this);
    var imgID = $img.attr('id');
